d7cc70885291b38838ed9242ce36a8454e2475cc
spatrik2001 <scheuer.patrik@gmail.com>
2024-11-25 23:41:03 +0100

    Commentek hozzáadása a hibás sorok felett

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+    // N_ELEMENTS helyett NELEMENTS van
     int *b = new int[NELEMENTS];
+    // A szöveget nem fogja megjeleníteni, mivel az '' egyetlen karakter kiiratásakor használatos
     std::cout << '1-100 ertekek duplazasa'
+    // Ebben a for ciklusban hiányzik a feltétel és az inkrementáló rész
     for (int i = 0;)
     {
         b[i] = i * 2;
@@ -16,6 +19,7 @@ int main()
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
+    // for ciklusnál a feltétel után pontosvessző helyett csak sima vessző szerepel
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
