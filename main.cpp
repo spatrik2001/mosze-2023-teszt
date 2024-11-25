a82a3850ea9c0bda3629c77e5ba7c81dd8534fbb
spatrik2001 <scheuer.patrik@gmail.com>
2024-11-25 23:59:30 +0100

    A programban előforduló hibák kijavítása

--- main.cpp
+++ main.cpp
@@ -5,24 +5,24 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     // N_ELEMENTS helyett NELEMENTS van
-    int *b = new int[NELEMENTS];
+    int *b = new int[N_ELEMENTS];
     // A szöveget nem fogja megjeleníteni, mivel az '' egyetlen karakter kiiratásakor használatos
-    std::cout << '1-100 ertekek duplazasa'
+    std::cout << "1-100 ertekek duplazasa";
     // Ebben a for ciklusban hiányzik a feltétel és az inkrementáló rész
-    for (int i = 0;)
+    for (int i = 0; i; i++)
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:";
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    // for ciklusnál a feltétel után pontosvessző helyett csak sima vessző szerepel
-    for (int i = 0; i < N_ELEMENTS, i++)
+    // for ciklusnál a feltétel után pontosvessző helyett csak sima vessző szerepel, illetve a b[i] után is lemaradt a pontosvessző
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
